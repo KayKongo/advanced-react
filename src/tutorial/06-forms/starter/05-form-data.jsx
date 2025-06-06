4bc17bab37d69916d68a70ba98c3d529f84e1bdf
kaykongo <pkkongo@st.ug.edu.gh>
2025-06-06 20:44:34 +0000

    feat: Learnt a bit about the FormData API

--- src/tutorial/06-forms/starter/05-form-data.jsx
+++ src/tutorial/06-forms/starter/05-form-data.jsx
@@ -1,43 +1,56 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const UncontrolledInputs = () => {
   const [value, setValue] = useState(0);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    // Initialise the FormData API
+    const formData = new FormData(e.currentTarget);
+
+    // How to get a value using a key ("name" should be set in the input/select fields) from FormData
+    const name = formData.get("name");
+
+    // Displaying the entries of the FormData
+    console.log([...formData.entries()]);
+
+    // COnverting FormData (which is an array of arrays) into objects -- some servers want JSON only
+    const objectData = Object.fromEntries(formData);
+    console.log(objectData);
   };
   return (
     <div>
-      <form className='form' onSubmit={handleSubmit}>
+      <form className="form" onSubmit={handleSubmit}>
         <h4>Form Data API</h4>
         {/* name */}
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type='text' className='form-input' id='name' name='name' />
+          <input type="text" className="form-input" id="name" name="name" />
         </div>
         {/* email */}
-        <div className='form-row'>
-          <label htmlFor='email' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="email" className="form-label">
             Email
           </label>
-          <input type='email' className='form-input' id='email' name='email' />
+          <input type="email" className="form-input" id="email" name="email" />
         </div>
-        {/* email */}
-        <div className='form-row'>
-          <label htmlFor='password' className='form-label'>
+        {/* password */}
+        <div className="form-row">
+          <label htmlFor="password" className="form-label">
             Password
           </label>
           <input
-            type='password'
-            className='form-input'
-            id='password'
-            name='password'
+            type="password"
+            className="form-input"
+            id="password"
+            name="password" // never forget aboout the "name" value when using formData
           />
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
