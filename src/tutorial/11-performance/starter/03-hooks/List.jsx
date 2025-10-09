d33d64bafd61d61347a55f4bcef24f8c4d96df61
kaykongo <pkkongo@st.ug.edu.gh>
2025-10-09 22:43:09 +0000

    feat: memoized a component with React.memo and memoized a function with useCallback

--- src/tutorial/11-performance/starter/03-hooks/List.jsx
+++ src/tutorial/11-performance/starter/03-hooks/List.jsx
@@ -1,12 +1,15 @@
-import Item from './Person';
+import Item from "./Person";
+import { memo } from "react";
 
-const List = ({ people }) => {
+const List = ({ people, removePerson }) => {
   return (
     <div>
       {people.map((person) => {
-        return <Item key={person.id} {...person} />;
+        return <Item key={person.id} {...person} removePerson={removePerson} />;
       })}
     </div>
   );
 };
-export default List;
+export default memo(List);
+
+// NOTE: React.memo checks for changes in the props of the component in its curly braces. If there's no change, it does not allow the said component to re-render.
--- src/tutorial/11-performance/starter/03-hooks/index.jsx
+++ src/tutorial/11-performance/starter/03-hooks/index.jsx
@@ -1,20 +1,30 @@
-import { useState } from 'react';
-import { data } from '../../../../data';
-import List from './List';
+import { useState, useCallback } from "react";
+import { data } from "../../../../data";
+import List from "./List";
+
 const LowerState = () => {
   const [people, setPeople] = useState(data);
   const [count, setCount] = useState(0);
 
+  // remove person functionality memoized with useCallback hook
+  const removePerson = useCallback(
+    (id) => {
+      const newPerson = people.filter((person) => person.id !== id);
+      setPeople(newPerson);
+    },
+    [people]
+  );
+
   return (
     <section>
       <button
-        className='btn'
+        className="btn"
         onClick={() => setCount(count + 1)}
-        style={{ marginBottom: '1rem' }}
+        style={{ marginBottom: "1rem" }}
       >
         count {count}
       </button>
-      <List people={people} />
+      <List people={people} removePerson={removePerson} />
     </section>
   );
 };
