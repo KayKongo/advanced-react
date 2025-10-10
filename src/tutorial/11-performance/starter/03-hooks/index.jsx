fbce9a87e7f1971538fcdec7687d906c43e2deb8
kaykongo <pkkongo@st.ug.edu.gh>
2025-10-10 02:12:27 +0000

    feat: learnt how to use useMemo hook to memoize values of really slow-to-calculate functions

--- src/tutorial/11-performance/starter/03-hooks/index.jsx
+++ src/tutorial/11-performance/starter/03-hooks/index.jsx
@@ -1,10 +1,15 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useMemo } from "react";
 import { data } from "../../../../data";
 import List from "./List";
+import slowFunction from "./slowFunction";
 
 const LowerState = () => {
   const [people, setPeople] = useState(data);
   const [count, setCount] = useState(0);
+
+  // slow function
+  const value = useMemo(() => slowFunction(), []);
+  console.log(value);
 
   // remove person functionality memoized with useCallback hook
   const removePerson = useCallback(
