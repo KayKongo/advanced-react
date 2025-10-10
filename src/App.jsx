730634abd978e766bea10ee99d159d2fedccb43d
kaykongo <pkkongo@st.ug.edu.gh>
2025-10-10 01:39:48 +0000

    feat: implement the fetch functionality using useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Starter from "./tutorial/11-performance/starter/03-hooks";
+// import Starter from "./tutorial/11-performance/starter/03-hooks";
+import Final from "./tutorial/02-useEffect/final/04-fetch-data";
 
 function App() {
   return (
     <div className="container">
-      <Starter />
+      {/* <Starter /> */}
+      <Final />
     </div>
   );
 }
